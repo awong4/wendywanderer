ef4a74a9893a7e62f1b9bca4ffc8a0a9f9a5baae
Carissa Sun <msun@wellesley.edu>
2015-04-24 00:57:37 -0400

    new version of posttrip

--- posttrip.php
+++ posttrip.php
@@ -6,37 +6,35 @@
 <body>
 <h1> Welcome to Wendy Wanderer - Post a Trip</h1>
 
-<!-- Search function -->
-
-<!-- id int auto_increment primary key,
-        ownerID int NOT NULL,
-        foreign key(ownerID) references user(id) on delete cascade,
-        startDate date,
-        endDate date,
-        cityID int NOT NULL, -->
 <p> This page is used to add a potential trip for the future
 <br> Please enter a start date, an end date, and the name of the city and country you're visiting
 
-<form method='post' action='<?php echo $_SERVER['PHP_SELF']; ?>'>
+<form method='GET' action='<?php echo $_SERVER['PHP_SELF']; ?>'>
 	<!-- we want users to be able to enter
-		 a start date, a finish date, and the city name -->
-        Start date: <input type="text" name="startdate" id='textinput'> <br>
-        End date: <input type="text" name="enddate" id='textinput'> <br>
-        City: <input type="text" name="cityname" id='textinput'> <br>
+		 a start date and a finish date -->
+        Start date: <input type="text" name='startdate' id='textinput'> <br>
+        End date: <input type="text" name='enddate' id='textinput'> <br>
+        <!-- ask the user to select a city from the list -->
+        City: <select name='city' id='tableselect'>
+        	<option value='novalue'> Please select a city
+	        <?php
+				require_once("MDB2.php");
+				require_once("/home/cs304/public_html/php/MDB2-functions.php");
+				require_once('wendy-dsn.inc');
+
+				$dbh = db_connect($wendy_dsn);
+				$sql = "SELECT name FROM city";
+				$cityname = query($dbh,$sql);
+
+				while($row = $cityname->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+				    $name = $row['name'];
+				    echo "<option value='$name'> $name\n";
+			 	}
+	        ?>
+    	</select>
 
         <input type="submit" value="submit" id='submitbutton'>
 </form>
-
-
-<!-- Get all the postings -->
-
-<p>
-<a href="home.html"> Home </a><br>
-<a href="signup.html"> Signup </a> <br>
-<a href="login.html"> Login </a> <br>
-<a href="findatrip.html"> Find a Trip </a> <br>
-<a href="recommendations.html"> Recommendations </a>
-
 
 <?php
 
@@ -49,41 +47,51 @@ $dbh = db_connect($wendy_dsn);
 insertTrip();
 
 function insertTrip(){
-	// check to see all the forms values are not null
-	if(isset($_GET['startdate']) and 
-		isset($_GET['enddate']) and 
-		isset($_GET['city'])){
-		global $dbh;
 
-		$cityID = getID(isset($_GET['city']));
-	    $sqlname = "INSERT INTO Trip values (?,?,?,?)";
-	    $owner = 0;
-	 	$start = $_GET['startdate'];
-	 	$end = $_GET['enddate'];
-	    $cID = $cityID;
-	    $values = array($owner,$start,$end,$cID);
-	    $resultname = prepared_query($dbh,$sqlname,$values);
+		if(isset($_GET['city']) and $_GET['city'] == 'novalue') {
+			echo "<p> Please select a city";
+		} else if(isset($_GET['startdate']) and isset($_GET['enddate'])){
+			global $dbh;
 
-	    
-	} else {
+		    $sql = "INSERT INTO trip(ownerID,startDate,endDate,cityID) values (?,?,?,?)";
+		    $owner = 1;
+		 	$start = $_GET['startdate'];
+		 	$end = $_GET['enddate'];
+		    $city = $_GET['city'];
+
+		    $result = getID($city);
+		    $row = $result->fetchRow(MDB2_FETCHMODE_ASSOC);
+		    $id = $row['id'];
+		    // echo "ID: $id";
+		    $values = array($owner,$start,$end,$id);
+		    $resultname = prepared_query($dbh,$sql,$values);
+		    // echo "$city";
+	    }
+	else {
 		 echo "<p> Please fill out all the form data fields";
 	}
 }
 
 function getID($searchedName){
-		$getcityid = "SELECT id FROM city where name like ?";
-        $input = "%".$searchedName."%"; /*allow for variations*/
-        $values = array($input);
-        global $dbh;
-        $result = prepared_query($dbh,$getcityid,$values);
-
-        $row = $result->fetchRow(MDB2_FETCHMODE_ASSOC);
-	    $id = $row['id'];
-        return $id;
+	global $dbh;
+	$getcityid = "SELECT id FROM city WHERE name like ?";
+    $input = "%".$searchedName."%"; /*allow for variations*/
+    $values = array($input);
+    $result = prepared_query($dbh,$getcityid,$values);
+    return $result;
 }
 
-
 ?>
+
+
+<!-- Get all the postings -->
+
+<p>
+<a href="home.html"> Home </a><br>
+<a href="signup.html"> Signup </a> <br>
+<a href="login.html"> Login </a> <br>
+<a href="findatrip.html"> Find a Trip </a> <br>
+<a href="recommendations.html"> Recommendations </a>
 
 </body>
 
